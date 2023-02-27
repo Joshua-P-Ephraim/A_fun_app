30af39324c4de64029e71032ec5c301040c80f6a
Joshua-P-Ephraim <126053389+Joshua-P-Ephraim@users.noreply.github.com>
2023-02-27 16:52:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title(":red[Innomatics] Data App :sunglasses:")
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+if btn_click == True:
+    st.subheader("I am Josh just starting my DS internship :+1:")
+    st.balloons()
